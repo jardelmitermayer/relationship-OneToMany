b69abd9767cae7f32ce19cf5d907305ebfb7c023
Jardel Mitermayer <jardel.filho@dcomp.ufs.br>
2021-05-06 03:47:37 -0300

    feat:adicionando belongsTo

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     use HasFactory;
+    function categoria(){
+        return $this->belongsTo('App\Models\Categoria');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Categoria;
+use App\Models\Produto;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,7 +26,26 @@ Route::get('categorias', function () {
     }
     else{
         foreach($cat as $c){
-            echo "<p>". $c->id."-". $c->nome ."/<p>";
+            echo "<p>". $c->id."-". $c->nome ."</p>";
+        }
+    }
+});
+Route::get('produtos', function () {
+    $prods = Produto::all();
+    if (count($prods) === 0) {
+        echo "<h4>Você não possui nenhuma categoria cadastrada</h4>";
+    }
+    else{
+        echo "<table>";
+        echo "<thead><tr><td>Nome</td><td>Estoque</td><td>Preco</td><td>Categoria</td><tr></thead>";
+        
+        foreach($prods as $prod){
+            echo "<tr>";
+            echo "<td>". $prod->nome . "</td>";
+            echo "<td>". $prod->estoque . "</td>";
+            echo "<td>". $prod->preco . "</td>";
+            echo "<td>". $prod->categoria->nome. "</td>";
+            echo "</tr>";
         }
     }
 });
